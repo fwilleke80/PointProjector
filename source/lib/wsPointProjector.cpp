76a545a01c0f31a2a332fc03a0698742e0d5b85c
Frank Willeke <frank.willeke@laubwerk.com>
2016-11-06 01:47:29 +0100

    Projection modes & bugfixes
    Implemented new projection mode "Spherical"
    Changed parallel projection direction to +Z
    Fixed bugs

--- source/lib/wsPointProjector.cpp
+++ source/lib/wsPointProjector.cpp
@@ -1,6 +1,7 @@
 #include "c4d.h"
 #include "wsPointProjector.h"
 
+
 // Initialize Projector with op as collision geometry
 // Caller owns the pointed object
 Bool wsPointProjector::Init(PolygonObject *collisionObject, Bool force)
@@ -34,13 +35,14 @@ Bool wsPointProjector::ProjectPosition(Vector &position, const Vector &rayDirect
 		return false;
 
 	GeRayColResult collisionResult;
-	Vector ppos(collisionObjectMgI * position);	// Transform position to m_collop's local space
+	Vector rPos(collisionObjectMgI * position);				// Transform position to m_collop's local space
+	Vector rDir(collisionObjectMgI.TransformVector(rayDirection));		// Transform direction to m_collop's local space
 
-	if (_collider->Intersect(ppos, rayDirection, rayLength, false))
+	if (_collider->Intersect(rPos, rDir, rayLength, false))
 	{
 		// Get collision result
 		if (!_collider->GetNearestIntersection(&collisionResult)) return false;
-
+		
 		// Transform position back to global space
 		position = collisionObjectMg * collisionResult.hitpos;
 	}
@@ -54,10 +56,8 @@ Bool wsPointProjector::Project(PointObject *op, const wsPointProjectorParams &pa
 	if (!_initialized || !_collider || !_collisionObject || !op)
 		return FALSE;
 	
-	Vector rayPosition(DC);
-
 	// Get point count
-	Int32 pointCount = op->GetPointCount();
+	const Int32 pointCount = op->GetPointCount();
 	if (pointCount <= 0)
 		return false;
 	
@@ -67,15 +67,18 @@ Bool wsPointProjector::Project(PointObject *op, const wsPointProjectorParams &pa
 		return false;
 	
 	// Get global Matrix of collision object and op (precalculated for better performance)
-	Matrix collisionObjectMg = _collisionObject->GetMg();
-	Matrix opMg = op->GetMg();
+	const Matrix collisionObjectMg = _collisionObject->GetMg();
+	const Matrix opMg = op->GetMg();
 	
 	// Also calculate the inversions of both matrices (precalculated for better performance)
-	Matrix collisionObjectMgI = ~collisionObjectMg;
-	Matrix opMgI = ~opMg;
+	const Matrix collisionObjectMgI = ~collisionObjectMg;
+	const Matrix opMgI = ~opMg;
 	
-	// Subdivide, if desired
-	/* TO DO */
+	// Ray direction
+	Vector rayPosition(DC);
+	
+	// Ray direction in global space
+	Vector rayDirection(params._modifierMg.TransformVector(Vector(0.0, 0.0, 1.0)));
 
 	// Calculate a ray length.
 	// The resulting length might be a bit too long, but with this we're on the safe side. No ray should ever be too short.
@@ -86,9 +89,15 @@ Bool wsPointProjector::Project(PointObject *op, const wsPointProjectorParams &pa
 	{
 		// Transform point position to global space
 		rayPosition = opMg * padr[i];
-
+		
+		// Calculate ray direction for spherical projection
+		if (params._mode == PROJECTORMODE_SPHERICAL)
+		{
+			rayDirection = rayPosition - params._modifierMg.off;
+		}
+		
 		// Project point
-		if (!ProjectPosition(rayPosition, params._direction, rayLength, collisionObjectMg, collisionObjectMgI))
+		if (!ProjectPosition(rayPosition, rayDirection, rayLength, collisionObjectMg, collisionObjectMgI))
 			return false;
 
 		// Transform point position back to op's local space
